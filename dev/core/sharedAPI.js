2e0c2672d412f5b81a14d69c2c35432af3863b18
DMHYT <danmarkhunter13072006@gmail.com>
2021-01-08 16:00:43 +0200

    Project structure rework

--- dev/core/sharedAPI.js
+++
@@ -1,9 +0,0 @@
-ModAPI.registerAPI("SolarFlux", {
-    Registry: SolarRegistry,
-    Connector: SolarConnector,
-    requireGlobal: function(command){
-        return eval(command);
-    }
-});
-
-Logger.Log("SolarFluxReborn API shared with name SolarFlux", "API");
---
+++ mod/SolarFluxRebornPE/launcher.js
@@ -0,0 +1,6 @@
+ConfigureMultiplayer({
+    name: "Solar Flux Reborn PE",
+    version: "0.2",
+    isClientOnly: false
+});
+Launch();
