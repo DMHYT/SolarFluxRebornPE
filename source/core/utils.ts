96049db4c21687b86ecc85947e2723adcda75cff
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-05-01 23:37:12 +0300

    New panel model connection logic

---
+++ source/core/utils.ts
@@ -0,0 +1,21 @@
+namespace SunUtils {
+
+    export function calculateCelestialAngle(worldTime: number, partialTicks: number): number {
+        let i: number = worldTime % 24000;
+        let f: number = (i + partialTicks) / 24000 - 0.25;
+        if(f < 0) ++f;
+        if(f > 1) --f;
+        let f1: number = 1 - (Math.cos(f * Math.PI) + 1) / 2;
+        f += (f1 - f) / 3;
+        return f;
+    }
+
+    export function getCelestialAngle(partialTicks: number): number {
+        return calculateCelestialAngle(World.getWorldTime(), partialTicks);
+    }
+
+    export function getCelestialAngleRadians(partialTicks: number): number {
+        return getCelestialAngle(partialTicks) * Math.PI * 2;
+    }
+    
+}
--- source/header.ts
+++ source/header.ts
@@ -1,19 +1,19 @@
-IMPORT("MinecraftUtils");
+/*
+ ██████╗ █████╗ ██╗      █████╗ ██████╗   ███████╗██╗     ██╗   ██╗██╗  ██╗  ██████╗ ███████╗██████╗  █████╗ ██████╗ ███╗  ██╗  ██████╗ ███████╗
+██╔════╝██╔══██╗██║     ██╔══██╗██╔══██╗  ██╔════╝██║     ██║   ██║╚██╗██╔╝  ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔══██╗████╗ ██║  ██╔══██╗██╔════╝
+╚█████╗ ██║  ██║██║     ███████║██████╔╝  █████╗  ██║     ██║   ██║ ╚███╔╝   ██████╔╝█████╗  ██████╦╝██║  ██║██████╔╝██╔██╗██║  ██████╔╝█████╗  
+ ╚═══██╗██║  ██║██║     ██╔══██║██╔══██╗  ██╔══╝  ██║     ██║   ██║ ██╔██╗   ██╔══██╗██╔══╝  ██╔══██╗██║  ██║██╔══██╗██║╚████║  ██╔═══╝ ██╔══╝  
+██████╔╝╚█████╔╝███████╗██║  ██║██║  ██║  ██║     ███████╗╚██████╔╝██╔╝╚██╗  ██║  ██║███████╗██████╦╝╚█████╔╝██║  ██║██║ ╚███║  ██║     ███████╗
+╚═════╝  ╚════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝  ╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝  ╚═╝  ╚═╝╚══════╝╚═════╝  ╚════╝ ╚═╝  ╚═╝╚═╝  ╚══╝  ╚═╝     ╚══════╝
+*/
 
-namespace SunUtils {
-    export function calculateCelestialAngle(worldTime: number, partialTicks: number): number {
-        let i: number = worldTime % 24000;
-        let f: number = (i + partialTicks) / 24000 - 0.25;
-        if(f < 0) ++f;
-        if(f > 1) --f;
-        let f1: number = 1 - (Math.cos(f * Math.PI) + 1) / 2;
-        f += (f1 - f) / 3;
-        return f;
-    }
-    export function getCelestialAngle(partialTicks: number): number {
-        return calculateCelestialAngle(World.getWorldTime(), partialTicks);
-    }
-    export function getCelestialAngleRadians(partialTicks: number): number {
-        return getCelestialAngle(partialTicks) * Math.PI * 2;
-    }
-}
+// © vstannumdum 2021
+// YouTube DMH (Russian)
+// YouTube vstannumdum (English)
+// My VK - https://www.vk.com/vstannumdum
+// Report bugs in VK Public - https://www.vk.com/dmhmods
+// Original Forge mod by Zeitheron and DragonForgeMC
+
+const BLOCK = ICRender.BLOCK;
+const AND = ICRender.AND;
+const NOT = ICRender.NOT;
