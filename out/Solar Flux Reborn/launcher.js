fe667a1f6cce49bf5723a42c6fbe30b0f38d75db
DaniGoodwin <danmarkhunter13072006@gmail.com>
2021-08-27 13:56:49 +0300

    Version 0.9.0 and added description to launcher

--- out/Solar Flux Reborn/launcher.js
+++ out/Solar Flux Reborn/launcher.js
@@ -1,6 +1,7 @@
 ConfigureMultiplayer({
     name: "Solar Flux Reborn BE",
-    version: "1.0",
-    isClientOnly: false
+    version: "0.9.0",
+    isClientOnly: false,
+    description: "Solar Flux Reborn adds solar panels that generate RF."
 });
 Launch();
