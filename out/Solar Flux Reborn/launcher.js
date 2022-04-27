77fe3479ad27fa583d47ed30d083a108736bb49b
vsdum <danmarkhunter13072006@gmail.com>
2022-04-27 15:19:00 +0300

    Version 0.9.3

--- out/Solar Flux Reborn/launcher.js
+++ out/Solar Flux Reborn/launcher.js
@@ -1,6 +1,6 @@
 ConfigureMultiplayer({
-    name: "Solar Flux Reborn BE",
-    version: "0.9.2",
+    name: "Solar Flux Reborn",
+    version: "0.9.3",
     isClientOnly: false,
     description: "Solar Flux Reborn adds solar panels that generate RF."
 });
